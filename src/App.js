bbeb536e174e4e85ca6aa0a250f81aa27dd99715
Ezra <ezra.rr.davis@gmail.com>
2020-08-10 16:03:41 -0700

    added chat route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { useContext, useEffect, useState } from "react";
-import Navbar from "./components/navbar.jsx";
-import Home from "./components/home/Home.jsx";
-import { Route, Switch, withRouter } from "react-router-dom";
-import Live from "./components/live/Live.jsx";
-import { FirebaseContext } from "./components/FirebaseWrapper.js";
-import Profile from "./components/profile/Profile.jsx";
-import { makeStyles, Chip, Button } from "@material-ui/core";
-import { grey } from "@material-ui/core/colors";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core";
-import themes from "./themes/index.js";
-import SiteWrapper, { SiteContext } from "./components/SiteWrapper.js";
-import { withSnackbar } from "notistack";
+import React, { useContext, useEffect, useState } from 'react';
+import Navbar from './components/navbar.jsx';
+import Home from './components/home/Home.jsx';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import Live from './components/live/Live.jsx';
+import { FirebaseContext } from './components/FirebaseWrapper.js';
+import Profile from './components/profile/Profile.jsx';
+import { makeStyles, Chip, Button } from '@material-ui/core';
+import { grey } from '@material-ui/core/colors';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+import themes from './themes/index.js';
+import SiteWrapper, { SiteContext } from './components/SiteWrapper.js';
+import { withSnackbar } from 'notistack';
 
 function App({ location, enqueueSnackbar }) {
   const { userStatus } = useContext(FirebaseContext);
@@ -24,6 +24,7 @@ function App({ location, enqueueSnackbar }) {
           <Switch>
             <Route exact strict path='/' component={Home} />
             <Route exact strict path='/live' component={Live} />
+            <Route exact strict path='/chat' render={(props) => <Live chatOnly {...props} />} />
             <Route exact strict path='/profile' component={Profile} />
           </Switch>
         </div>
